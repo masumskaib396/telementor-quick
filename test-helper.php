4add4823c40513b67c537cfe67a31bb9582ba6f3
Masum Sakib <masumsakib396@gmail.com>
2021-10-20 21:13:01 +0600

    first commit

---
+++ test-helper.php
@@ -0,0 +1,286 @@
+<?php
+
+/**
+ * Plugin Name: Test
+ * Description: This plugin is compatible with test WordPress Landing Page Theme
+ * Plugin URI:
+ * Version:     1.0.0
+ * Author:      Masum Sakib
+ * Author URI:
+ * Text Domain: test-helper
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+/**
+ * Main test Class
+ *
+ * The main class that initiates and runs the plugin.
+ *
+ * @since 1.0.0
+ */
+final class test_elementor
+{
+
+    /**
+     * Plugin Version
+     *
+     * @since 1.0.0
+     *
+     * @var string The plugin version.
+     */
+    const VERSION = '1.0.0';
+
+    /**
+     * Minimum Elementor Version
+     *
+     * @since 1.0.0
+     *
+     * @var string Minimum Elementor version required to run the plugin.
+     */
+    const MINIMUM_ELEMENTOR_VERSION = '2.0.0';
+
+    /**
+     * Minimum PHP Version
+     *
+     * @since 1.0.0
+     *
+     * @var string Minimum PHP version required to run the plugin.
+     */
+    const MINIMUM_PHP_VERSION = '5.6';
+
+    /**
+     * Instance
+     *
+     * @since 1.0.0
+     *
+     * @access private
+     * @static
+     *
+     * @var test_elementor The single instance of the class.
+     */
+    private static $_instance = null;
+
+    /**
+     * Instance
+     *
+     * Ensures only one instance of the class is loaded or can be loaded.
+     *
+     * @since 1.0.0
+     *
+     * @access public
+     * @static
+     *
+     * @return test_elementor An instance of the class.
+     */
+    public static function instance()
+    {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+
+    /**
+     * Constructor
+     *
+     * @since 1.0.0
+     *
+     * @access public
+     */
+    public function __construct()
+    {
+        add_action('init', [$this, 'i18n']);
+        add_action('plugins_loaded', [$this, 'init']);
+    }
+
+    /**
+     * Load Textdomain
+     *
+     * Load plugin localization files.
+     *
+     * Fired by `init` action hook.
+     *
+     * @since 1.0.0
+     *
+     * @access public
+     */
+    public function i18n()
+    {
+        load_plugin_textdomain('test');
+    }
+
+    /**
+     * Initialize the plugin
+     *
+     * Load the plugin only after Elementor (and other plugins) are loaded.
+     * Checks for basic plugin requirements, if one check fail don't continue,
+     * if all check have passed load the files required to run the plugin.
+     *
+     * Fired by `plugins_loaded` action hook.
+     *
+     * @since 1.0.0
+     *
+     * @access public
+     */
+    public function init()
+    {
+
+
+
+
+        // Check if Elementor installed and activated
+        if (!did_action('elementor/loaded')) {
+            add_action('admin_notices', [$this, 'admin_notice_missing_main_plugin']);
+            return;
+        }
+
+        // Check for required Elementor version
+        if (!version_compare(ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=')) {
+            add_action('admin_notices', [$this, 'admin_notice_minimum_elementor_version']);
+            return;
+        }
+
+        // Check for required PHP version
+        if (version_compare(PHP_VERSION, self::MINIMUM_PHP_VERSION, '<')) {
+            add_action('admin_notices', [$this, 'admin_notice_minimum_php_version']);
+            return;
+        }
+
+        // Add Plugin actions
+        add_action('elementor/widgets/widgets_registered', [$this, 'init_widgets']);
+        add_action('elementor/frontend/after_enqueue_styles', [$this, 'widget_styles']);
+        add_action('elementor/frontend/after_register_scripts', [$this, 'widget_scripts']);
+    }
+
+
+
+    /**
+     * Admin notice
+     *
+     * Warning when the site doesn't have Elementor installed or activated.
+     *
+     * @since 1.0.0
+     *
+     * @access public
+     */
+    public function admin_notice_missing_main_plugin()
+    {
+        if (isset($_GET['activate'])) {
+            unset($_GET['activate']);
+        }
+
+        $message = sprintf(
+            /* translators: 1: Plugin name 2: Elementor */
+            esc_html__('"%1$s" requires "%2$s" to be installed and activated.', 'test'),
+            '<strong>' . esc_html__('test Helper', 'test') . '</strong>',
+            '<strong>' . esc_html__('Elementor', 'test') . '</strong>'
+        );
+
+        printf('<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message);
+    }
+
+    /**
+     * Admin notice
+     *
+     * Warning when the site doesn't have a minimum required Elementor version.
+     *
+     * @since 1.0.0
+     *
+     * @access public
+     */
+    public function admin_notice_minimum_elementor_version()
+    {
+        if (isset($_GET['activate'])) {
+            unset($_GET['activate']);
+        }
+
+        $message = sprintf(
+            /* translators: 1: Plugin name 2: Elementor 3: Required Elementor version */
+            esc_html__('"%1$s" requires "%2$s" version %3$s or greater.', 'test'),
+            '<strong>' . esc_html__('test Helper', 'test') . '</strong>',
+            '<strong>' . esc_html__('Elementor', 'test') . '</strong>',
+            self::MINIMUM_ELEMENTOR_VERSION
+        );
+
+        printf('<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message);
+    }
+
+    /**
+     * Admin notice
+     *
+     * Warning when the site doesn't have a minimum required PHP version.
+     *
+     * @since 1.0.0
+     *
+     * @access public
+     */
+    public function admin_notice_minimum_php_version()
+    {
+        if (isset($_GET['activate'])) {
+            unset($_GET['activate']);
+        }
+
+        $message = sprintf(
+            /* translators: 1: Plugin name 2: PHP 3: Required PHP version */
+            esc_html__('"%1$s" requires "%2$s" version %3$s or greater.', 'test'),
+            '<strong>' . esc_html__('test', 'test') . '</strong>',
+            '<strong>' . esc_html__('PHP', 'test') . '</strong>',
+            self::MINIMUM_PHP_VERSION
+        );
+
+        printf('<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message);
+    }
+
+    /**
+     * Init Widgets
+     *
+     * Include widgets files and register them
+     *
+     * @since 1.0.0
+     *
+     * @access public
+     */
+    public function init_widgets()
+    {
+
+        $widgets_manager = \Elementor\Plugin::instance()->widgets_manager;
+
+
+        /* Test Widget */
+         require_once __DIR__ . '/widgets/test-widget';
+
+
+    }
+
+    // Adding Your Style File
+    public function widget_styles()
+    {
+
+    }
+
+    // Adding Your Js File
+    public function widget_scripts()
+    {
+
+    }
+}
+
+test_elementor::instance();
+
+/**
+ * adding new category
+ */
+function test_add_elementor_widget_categories($elements_manager)
+{
+    $elements_manager->add_category(
+        'test-addons',
+        [
+            'title' => __('Test addons', 'test'),
+            'icon' => 'fa fa-plug',
+        ]
+    );
+}
+add_action('elementor/elements/categories_registered', 'test_add_elementor_widget_categories');
